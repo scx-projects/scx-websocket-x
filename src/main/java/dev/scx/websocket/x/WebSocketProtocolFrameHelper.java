28d8bf7837b6cd1c276eccc3d3dfd2df6be7bba9
scx567888 <scx567888@outlook.com>
2026-02-05 17:32:04 +0800

    dev

--- src/main/java/dev/scx/websocket/x/WebSocketProtocolFrameHelper.java
+++ src/main/java/dev/scx/websocket/x/WebSocketProtocolFrameHelper.java
@@ -6,8 +6,6 @@ import dev.scx.io.exception.*;
 import dev.scx.websocket.WebSocketOpCode;
 import dev.scx.websocket.exception.WebSocketException;
 
-import java.util.ArrayList;
-
 import static dev.scx.websocket.close_info.WebSocketCloseInfo.TOO_BIG;
 
 /// WebSocketProtocolFrameHelper
@@ -15,22 +13,22 @@ import static dev.scx.websocket.close_info.WebSocketCloseInfo.TOO_BIG;
 /// @author scx567888
 /// @version 0.0.1
 /// @see <a href="https://www.rfc-editor.org/rfc/rfc6455">https://www.rfc-editor.org/rfc/rfc6455</a>
-public class WebSocketProtocolFrameHelper {
+public final class WebSocketProtocolFrameHelper {
 
-    public static WebSocketProtocolFrame readFrameHeader(ByteInput byteInput) throws NoMoreDataException, ScxInputException, InputAlreadyClosedException {
-        byte[] header = byteInput.readFully(2);
+    public static WebSocketProtocolFrame readProtocolFrameHeader(ByteInput byteInput) {
+        var protocolFrame = new WebSocketProtocolFrame();
 
-        var b1 = header[0];
+        byte b1 = byteInput.read();
 
-        var fin = (b1 & 0b1000_0000) != 0;
-        var rsv1 = (b1 & 0b0100_0000) != 0;
-        var rsv2 = (b1 & 0b0010_0000) != 0;
-        var rsv3 = (b1 & 0b0001_0000) != 0;
-        var opCode = WebSocketOpCode.of(b1 & 0b0000_1111);
+        protocolFrame.fin = (b1 & 0b1000_0000) != 0;
+        protocolFrame.rsv1 = (b1 & 0b0100_0000) != 0;
+        protocolFrame.rsv2 = (b1 & 0b0010_0000) != 0;
+        protocolFrame.rsv3 = (b1 & 0b0001_0000) != 0;
+        protocolFrame.opCode = WebSocketOpCode.of(b1 & 0b0000_1111);
 
-        var b2 = header[1];
+        byte b2 = byteInput.read();
 
-        var masked = (b2 & 0b1000_0000) != 0;
+        protocolFrame.masked = (b2 & 0b1000_0000) != 0;
         int payloadLength = b2 & 0b0111_1111;
 
         // 读取扩展长度
@@ -51,16 +49,16 @@ public class WebSocketProtocolFrameHelper {
                 extendedPayloadLength[7] & 0b1111_1111);
         }
 
-        byte[] maskingKey = null;
+        protocolFrame.payloadLength = payloadLength;
 
-        if (masked) {
-            maskingKey = byteInput.readFully(4);
+        if (protocolFrame.masked) {
+            protocolFrame.maskingKey = byteInput.readFully(4);
         }
 
-        return new WebSocketProtocolFrame(fin, rsv1, rsv2, rsv3, opCode, masked, payloadLength, maskingKey);
+        return protocolFrame;
     }
 
-    public static WebSocketProtocolFrame readFramePayload(WebSocketProtocolFrame frame, ByteInput byteInput) throws NoMoreDataException, ScxInputException, InputAlreadyClosedException {
+    public static WebSocketProtocolFrame readProtocolFramePayload(WebSocketProtocolFrame frame, ByteInput byteInput) throws NoMoreDataException, ScxInputException, InputAlreadyClosedException {
         var payloadLength = frame.payloadLength();
         var masked = frame.masked();
         var maskingKey = frame.maskingKey();
@@ -77,68 +75,19 @@ public class WebSocketProtocolFrameHelper {
         return frame.payloadData(payloadData);
     }
 
-    //读取单个帧
-    public static WebSocketProtocolFrame readFrame(ByteInput byteInput, long maxWebSocketFrameSize) throws NoMoreDataException, ScxInputException, InputAlreadyClosedException, WebSocketException {
-        var webSocketFrame = readFrameHeader(byteInput);
+    /// 读取单个帧
+    public static WebSocketProtocolFrame readProtocolFrame(ByteInput byteInput, long maxWebSocketFrameSize) throws NoMoreDataException, ScxInputException, InputAlreadyClosedException, WebSocketException {
+        var webSocketFrame = readProtocolFrameHeader(byteInput);
 
         //这里检查 最大帧大小
         if (webSocketFrame.payloadLength() > maxWebSocketFrameSize) {
             throw new WebSocketException(TOO_BIG.code() + "Frame too big");
         }
 
-        return readFramePayload(webSocketFrame, byteInput);
+        return readProtocolFramePayload(webSocketFrame, byteInput);
     }
 
-    public static WebSocketProtocolFrame readFrameUntilLast(ByteInput byteInput, long maxWebSocketFrameSize, long maxWebSocketMessageSize) throws NoMoreDataException, ScxInputException, InputAlreadyClosedException, WebSocketException {
-        var frameList = new ArrayList<WebSocketProtocolFrame>();
-        long totalPayloadLength = 0;
-
-        while (true) {
-            var webSocketFrame = readFrameHeader(byteInput);
-            var framePayloadLength = webSocketFrame.payloadLength();
-
-            // 检查单个帧大小限制
-            if (framePayloadLength > maxWebSocketFrameSize) {
-                throw new WebSocketException(TOO_BIG.code() + "Frame too big");
-            }
-
-            // 检查合并后的消息大小限制
-            if (totalPayloadLength + framePayloadLength > maxWebSocketMessageSize) {
-                throw new WebSocketException(TOO_BIG.code() + "Message too big");
-            }
-
-            webSocketFrame = readFramePayload(webSocketFrame, byteInput);
-
-            // 增加当前帧的有效负载长度
-            totalPayloadLength += framePayloadLength;
-
-            frameList.add(webSocketFrame);
-            if (webSocketFrame.fin()) {
-                break;
-            }
-        }
-
-        var first = frameList.get(0);
-
-        if (frameList.size() == 1) {
-            return first;
-        }
-
-        var opCode = first.opCode();
-        var length = frameList.stream().mapToInt(WebSocketProtocolFrame::payloadLength).sum();
-        var payloadData = new byte[length];
-
-        int offset = 0;
-        for (var webSocketFrame : frameList) {
-            System.arraycopy(webSocketFrame.payloadData(), 0, payloadData, offset, webSocketFrame.payloadLength());
-            offset += webSocketFrame.payloadLength();
-        }
-
-        return WebSocketProtocolFrame.of(true, opCode, payloadData);
-
-    }
-
-    public static void writeFrame(WebSocketProtocolFrame frame, ByteOutput byteOutput) throws ScxOutputException, OutputAlreadyClosedException {
+    public static void writeProtocolFrame(WebSocketProtocolFrame frame, ByteOutput byteOutput) throws ScxOutputException, OutputAlreadyClosedException {
         // 头部
         int fullOpCode = (frame.fin() ? 0b1000_0000 : 0) |
             (frame.rsv1() ? 0b0100_0000 : 0) |
