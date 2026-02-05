b8d0d5cc6db5e0363972407871149c95919c55d9
scx567888 <scx567888@outlook.com>
2026-02-05 18:20:51 +0800

    dev

--- src/test/java/dev/scx/websocket/x/test/WebSocketProtocolFrameHelperTest.java
+++ src/test/java/dev/scx/websocket/x/test/WebSocketProtocolFrameHelperTest.java
@@ -2,9 +2,10 @@ package dev.scx.websocket.x.test;
 
 import dev.scx.io.exception.NoMoreDataException;
 import dev.scx.io.output.ByteArrayByteOutput;
+import dev.scx.websocket.WebSocketFrame;
 import dev.scx.websocket.WebSocketOpCode;
 import dev.scx.websocket.exception.WebSocketException;
-import dev.scx.websocket.x.WebSocketProtocolFrame;
+import dev.scx.websocket.x.WebSocketHelper;
 import dev.scx.websocket.x.WebSocketProtocolFrameHelper;
 import org.testng.annotations.Test;
 
@@ -16,7 +17,6 @@ public class WebSocketProtocolFrameHelperTest {
     public static void main(String[] args) throws NoMoreDataException, WebSocketException {
         testReadFrame();
         testWriteFrame();
-        testReadFrameUntilLast();
     }
 
     @Test
@@ -27,52 +27,30 @@ public class WebSocketProtocolFrameHelperTest {
             'H', 'e', 'l', 'l', 'o'
         };
         var reader = createByteInput(frameData);
-        var frame = WebSocketProtocolFrameHelper.readFrame(reader, Integer.MAX_VALUE);
+        var frame = WebSocketProtocolFrameHelper.readProtocolFrame(reader, Integer.MAX_VALUE);
 
-        assertTrue(frame.fin());
-        assertFalse(frame.rsv1());
-        assertFalse(frame.rsv2());
-        assertFalse(frame.rsv3());
-        assertEquals(frame.opCode(), WebSocketOpCode.TEXT);
-        assertFalse(frame.masked());
-        assertEquals(frame.payloadLength(), 5);
-        assertEquals(frame.payloadData(), new byte[]{'H', 'e', 'l', 'l', 'o'});
+        assertTrue(frame.fin);
+        assertFalse(frame.rsv1);
+        assertFalse(frame.rsv2);
+        assertFalse(frame.rsv3);
+        assertEquals(frame.opCode, WebSocketOpCode.TEXT);
+        assertFalse(frame.masked);
+        assertEquals(frame.payloadLength, 5);
+        assertEquals(frame.payloadData, new byte[]{'H', 'e', 'l', 'l', 'o'});
     }
 
     @Test
     public static void testWriteFrame() {
-        var frame = WebSocketProtocolFrame.of(true, WebSocketOpCode.TEXT, new byte[]{'H', 'e', 'l', 'l', 'o'});
+        var frame = new WebSocketFrame(WebSocketOpCode.TEXT, new byte[]{'H', 'e', 'l', 'l', 'o'}, true);
+        var protocolFrame = WebSocketHelper.toProtocolFrame(frame, false);
         var out = new ByteArrayByteOutput();
-        WebSocketProtocolFrameHelper.writeFrame(frame, out);
+        WebSocketProtocolFrameHelper.writeProtocolFrame(protocolFrame, out);
         byte[] expectedFrame = {
             (byte) 0b1000_0001, // FIN + Text frame
             (byte) 0b0000_0101, // No mask, payload length = 5
             'H', 'e', 'l', 'l', 'o'
         };
         assertEquals(expectedFrame, out.bytes());
-    }
-
-    @Test
-    public static void testReadFrameUntilLast() throws NoMoreDataException, WebSocketException {
-        byte[] frameData = {
-            (byte) 0b0000_0001, // Text frame, not final
-            (byte) 0b0000_0011, // No mask, payload length = 3
-            'H', 'e', 'l',
-            (byte) 0b1000_0000, // Continuation frame, final
-            (byte) 0b0000_0010, // No mask, payload length = 2
-            'l', 'o'
-        };
-        var reader = createByteInput(frameData);
-        var frame = WebSocketProtocolFrameHelper.readFrameUntilLast(reader, Integer.MAX_VALUE, Integer.MAX_VALUE);
-
-        assertTrue(frame.fin());
-        assertFalse(frame.rsv1());
-        assertFalse(frame.rsv2());
-        assertFalse(frame.rsv3());
-        assertEquals(frame.opCode(), WebSocketOpCode.TEXT);
-        assertFalse(frame.masked());
-        assertEquals(frame.payloadLength(), 5);
-        assertEquals(frame.payloadData(), new byte[]{'H', 'e', 'l', 'l', 'o'});
     }
 
 }
